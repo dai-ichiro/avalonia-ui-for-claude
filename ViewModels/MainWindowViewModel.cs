7732f146a4cd2fc3968c3e6308cae8c53eca2031
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2026-03-09 21:19:22 +0900

    Add files via upload

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MyGuiApp.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
